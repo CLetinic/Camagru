a7a2d6e320012557b822ced0f915048497e803e4
cletinic <chanelletinic@gmail.com>
2018-11-19 13:09:10 +0200

    image upload working, need to double check on gallery

--- php/saveimg.php
+++ php/saveimg.php
@@ -13,15 +13,24 @@ include '../config/database.php';
 		{
 			if (isset($_POST['key']))
 			{
-				//$user_name	= $_SESSION['username'];
+				$user_name	= $_SESSION['username'];
+
+				$conn = new PDO("$DB_DNS;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD);
+				$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+				$sql = "USE ".$DB_NAME;
+				$stmt = $conn->prepare("SELECT * FROM users WHERE user_name=:user_name");
+				$stmt->bindValue(':user_name', $user_name);
+				$stmt->execute();
+				$usernames = $stmt->fetch();
+
+				if (!$usernames)
+					die("username does not exist");
+
+				$user_id	= $usernames['user_id'];				
 				$imageurl	= $_POST['key'];
-				$img		= preg_replace('#^data:image/\w+;base64,#i', '',$imageurl);
-				
+				$img		= preg_replace('#^data:image/\w+;base64,#i', '',$imageurl);				
 				$arr = array();
-
-				//$dest = imagecreatefromstring(base64_decode($img));
-				//$src = imagecreatefromstring(base64_decode(file_get_contents('bird.txt')));
-
+				
 				$index = 0;
 				for ($x = 0; $x <= 7; $x++) 
 				{
@@ -32,86 +41,42 @@ include '../config/database.php';
 					}
 				} 
 								
-				if (sizeof($arr) > 2)
-				{
+				for ($x = 0; $x < sizeof($arr); $x++)
+				{	
 					$dest = imagecreatefromstring(base64_decode($img));
-					$src = imagecreatefromstring(base64_decode($arr[0]));
-
-					// 	$src = imagecreatefromstring(base64_decode($arr[$x + 1]));
-					// for ($x = 0; $x + 1 < sizeof($arr) - 1; $x++)
-					// {	
-					// 	$dest = imagecreatefromstring(base64_decode($arr[$x]));
-					// 	$src = imagecreatefromstring(base64_decode($arr[$x + 1]));
+					$src = imagecreatefromstring(base64_decode($arr[$x]));
 
 					imagecopy($dest, $src, 0, 0, 0, 0, 600, 450);
 					header('Content-type: image/png');
 
-						ob_start();
-						imagepng($dest);
-						$sticker = ob_get_contents();
-						ob_end_clean();
+					ob_start();
+					imagepng($dest);
+					// Capture the output
+					$img = ob_get_contents();
 
-						$sticker = base64_encode($sticker);
-						$temp = base64_decode($sticker);
-						file_put_contents('join.png', $temp);
+					// Clear the output buffer
+					ob_end_clean();
 
-					// 	$arr[$x + 1] = base64_encode($arr[$x + 1]);
-					// 	file_put_contents('join'.$x.'.png', $arr[sizeof($arr) - 1]);
-					// }
-					// file_put_contents('join.png', $arr[sizeof($arr) - 1]);
-					
+					$img = base64_encode($img);
+					$temp = base64_decode($img);					
+
+					file_put_contents('join'.$x.'.png', $temp);
 				}
+				$temp = base64_decode($img);
+				file_put_contents('join.png', $temp);
+			
+				$sql = "INSERT INTO images (user_id, content)
+				VALUES (:user_id, :content)";
+				$stmt = $conn->prepare($sql);
+				$stmt->bindParam(':user_id', $user_id);
+				$stmt->bindParam(':content', $img);
+				$stmt->execute();
 
-				// if ($index > 2)
-				// {
-				// 	for (var $i = 0; $i <= $index; $i++)
-				// 	{
-				// 		$dest = imagecreatefromstring(base64_decode($_SESSION['num'.$imagenum]));
-				// 		$src = imagecreatefromstring(base64_decode($_SESSION['num'.$imagenum + 1]));
-				// 	}
-				// }
-				// else if ($index == 1)
-				// {
-
-				// }
-				// else if ($index == 0)
-				// {
-
-				// }
-
-				// imagecopy($dest, $src, 0, 0, 0, 0, 600, 450);
-				// header('Content-type: image/png');
-
-				// //https://stackoverflow.com/questions/9370847/php-create-image-with-imagepng-and-convert-with-base64-encode-in-a-single-file
-				// ob_start();
-				// 	imagepng($dest);
-				// 	// Capture the output
-				// 	$img = ob_get_contents();
-
-				// // Clear the output buffer
-				// ob_end_clean();
-
-				// $img = base64_encode($img);
-
-				// $conn = new PDO("$DB_DNS;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD);
-				// $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-				// $sql = "USE ".$DB_NAME;
-				// $stmt = $conn->prepare("SELECT * FROM users WHERE user_name=:user_name");
-				// $stmt->bindValue(':user_name', $user_name);
-				// $stmt->execute();
-				// $usernames = $stmt->fetch();
-
-				// if (!$usernames)
-				// 	die("username does not exist");
-
-				// $user_id	= $usernames['user_id'];
-
-				// $sql = "INSERT INTO images (user_id, content)
-				// VALUES (:user_id, :content)";
-				// $stmt = $conn->prepare($sql);
-				// $stmt->bindParam(':user_id', $user_id);
-				// $stmt->bindParam(':content', $img);
-				// $stmt->execute();
+				for ($x = 0; $x < sizeof($arr); $x++)
+				{
+					if (isset($_SESSION['num'.$x]))
+						unset($_SESSION['num'.$x]);
+				} 
 			}
 		}
 	}
